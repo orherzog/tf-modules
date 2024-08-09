cad23393b6f659d8c8e7b34d87996349957194f8
orherzog <orh@comm-it.com>
2024-08-09 22:37:00 +0700

    update

--- cicd/jenkins/key_pair.tf
+++ cicd/jenkins/key_pair.tf
@@ -1,7 +1,7 @@
 # Create EC2 Key Pair
 resource "aws_key_pair" "ec2_key_pair" {
   key_name   = var.key_name
-  public_key = tls_public_key.key.public_key_openssh
+  public_key = data.tls_public_key.private_key_pem
 }
 
 # Create a TLS key pair to generate a public/private key pair
