07e39535cbf9d56830d39e9a37d7e56aac95b176
orherzog <orh@comm-it.com>
2024-08-09 22:53:17 +0700

    update moduls

--- networking/vpc/main.tf
+++ networking/vpc/main.tf
@@ -12,11 +12,21 @@ module "vpc" {
   # Use the selected availability zones
   azs = local.azs
 
-  # Create private subnets in each selected AZ
-  private_subnets = [for k in range(length(local.azs)) : cidrsubnet(var.vpc_cidr, 4, k)]
-}
+  public_subnets   = [for k in range(length(local.azs)) : cidrsubnet(var.vpc_cidr, 4, k)]
+  private_subnets  = [for k in range(length(local.azs)) : cidrsubnet(var.vpc_cidr, 4, k + 4)]
+  database_subnets = [for k in range(length(local.azs)) : cidrsubnet(var.vpc_cidr, 4, k + 8)]
 
-# Output the selected availability zones for debugging
-output "selected_azs" {
-  value = local.azs
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  # VPC Flow Logs (Cloudwatch log group and IAM role will be created)
+  vpc_flow_log_iam_role_name            = "vpc-complete-example-role"
+  vpc_flow_log_iam_role_use_name_prefix = false
+  enable_flow_log                       = true
+  create_flow_log_cloudwatch_log_group  = true
+  create_flow_log_cloudwatch_iam_role   = true
+  flow_log_max_aggregation_interval     = 60
 }
