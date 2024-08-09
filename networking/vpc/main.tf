ead7bcbc2b3825b814a7d4e2418cc85a3217e8f4
orherzog <orh@comm-it.com>
2024-08-09 21:42:34 +0700

    debug

--- networking/vpc/main.tf
+++ networking/vpc/main.tf
@@ -9,6 +9,9 @@ module "vpc" {
   name = var.env
   cidr = var.vpc_cidr
 
-  azs             = local.azs
+  # Use the selected availability zones
+  azs = local.azs
+
+  # Create private subnets in each selected AZ
   private_subnets = [for k in local.azs : cidrsubnet(var.vpc_cidr, 4, index(local.azs, k))]
 }
